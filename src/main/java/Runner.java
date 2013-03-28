cf258d9a39e0108596ede566ede1e285d84f0b89
koosha <koosha.mirhosseini@gmail.com>
2013-03-28 15:50:49 -0400

    Project init

---
+++ src/main/java/Runner.java
@@ -0,0 +1,13 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: koosha
+ * Date: 3/21/13
+ * Time: 7:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Runner {
+
+    public static void main(String args[]){
+
+    }
+}
